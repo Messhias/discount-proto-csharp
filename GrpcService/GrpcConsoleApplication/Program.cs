6c14d85bde0921d11cea10d3c9123cfbcc79faa6
Fabio William Conceição <messhias@gmail.com>
2024-09-20 20:24:45 +0100

    created console test application

---
+++ GrpcService/GrpcConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcService.Protos;
+
+// Create the channel
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+// Create the client in the channel
+var client = new DiscountService.DiscountServiceClient(channel);
+
+// We make the request for discount codes.
+var generateResponse = await client.GenerateDiscountAsync(new GenerateRequest { Count = 5, Length = 8 });
+Console.WriteLine("Generated codes:");
+foreach (var code in generateResponse.Codes)
+{
+	Console.WriteLine(code); // show me the codes :) 
+}
+
+// Now let's  use our codes, right?!
+foreach (var code in generateResponse.Codes)
+{
+	var useResponse = await client.UseDiscountAsync(new UseRequest { Code = code });
+	Console.WriteLine($"Code {code} used successfully: {useResponse.Success}");
+}
+
